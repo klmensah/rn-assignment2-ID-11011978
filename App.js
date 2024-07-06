8062c7369e3eefc9a4f96f1bf550ed56b493f0f7
klmensah <lkmensah011@st.ug.edu.gh>
2024-07-06 12:19:14 +0000

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>My name is Lawrence</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f0f0f0', // Change the background color here
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24, // Increase font size
+    fontWeight: 'bold', // Make text bold
   },
 });
+
+export default App;
